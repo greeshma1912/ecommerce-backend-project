670ab1320bff015cf5ef343a828b2a7a7a827247
Greeshma <gajwadagreeshma@gmail.com>
2026-02-28 18:04:48 +0530

    Fixed authMiddleware

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware = require('../middleware/auth');
 
 router.get('/', productController.getAllProducts);
 router.post('/', authMiddleware, productController.createProduct);
