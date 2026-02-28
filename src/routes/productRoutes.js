9f38e0f5b4d19f852ae0bb75bdc2eb670dcb3ec6
Greeshma <gajwadagreeshma@gmail.com>
2026-02-28 18:00:00 +0530

    Fixed authMiddleware import error

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 router.get('/', productController.getAllProducts);
 router.post('/', authMiddleware, productController.createProduct);
